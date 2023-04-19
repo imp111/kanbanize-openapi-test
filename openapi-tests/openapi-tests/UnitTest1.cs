0ac46254f04ec3b2d4bf8fced0d627b0f4ff0497
imp111 <alexsandartenev@gmail.com>
2023-04-19 16:14:22 +0300

    get all workspaces

--- openapi-tests/openapi-tests/UnitTest1.cs
+++ openapi-tests/openapi-tests/UnitTest1.cs
@@ -1,3 +1,7 @@
+using RestSharp;
+using RestSharp.Authenticators;
+using RestSharp.Authenticators.OAuth2;
+
 namespace openapi_tests
 {
     public class UnitTest1
@@ -5,7 +9,18 @@ namespace openapi_tests
         [Fact]
         public void Test1()
         {
+            var restClient = new RestClient(new RestClientOptions
+            {
+                BaseUrl = new Uri("https://none7t.kanbanize.com/api/v2"),
+                Authenticator = new HttpBasicAuthenticator("alexsandartenev@gmail.com", "praseta123")
+            });
 
+            // request
+            var request = new RestRequest("/workspaces/");
+            request.AddHeader("apikey", "3ZIPG0qqf7fBuUQ8uqCt7N7iTKoGuOhHSwRRwdtd");
+
+           // response
+           var repsone = restClient.Get(request);
         }
     }
 }
