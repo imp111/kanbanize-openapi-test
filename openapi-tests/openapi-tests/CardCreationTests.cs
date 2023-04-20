25cb0605e4d3ab10588edd588b7eed5add660a3c
imp111 <alexsandartenev@gmail.com>
2023-04-20 07:51:56 +0300

    get all workspaces test

--- openapi-tests/openapi-tests/CardCreationTests.cs
+++ openapi-tests/openapi-tests/CardCreationTests.cs
@@ -3,7 +3,31 @@ namespace openapi_tests
     public class CardCreationTests
     {
         [Fact]
-        public void Test1()
+        public void GetAllWorkspaces()
+        {
+            var restClient = new RestClient(new RestClientOptions
+            {
+                BaseUrl = new Uri("https://none7t.kanbanize.com/api/v2"),
+                Authenticator = new HttpBasicAuthenticator("alexsandartenev@gmail.com", "praseta123")
+            });
+
+            // request
+            var request = new RestRequest("/workspaces");
+            request.AddHeader("apikey", "3ZIPG0qqf7fBuUQ8uqCt7N7iTKoGuOhHSwRRwdtd");
+
+            // response
+            var repsone = restClient.Get(request);
+            string statusCode = repsone.StatusCode.ToString();
+
+            // Assert
+            Assert.Equal("OK", statusCode);
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        public void GetWorkspaceById(int id)
         {
             var restClient = new RestClient(new RestClientOptions
             {
@@ -13,12 +37,16 @@ namespace openapi_tests
 
             // request
             var request = new RestRequest("/workspaces/{workspace_id}")
-                .AddUrlSegment("workspace_id", 2);
+                .AddUrlSegment("workspace_id", id);
 
             request.AddHeader("apikey", "3ZIPG0qqf7fBuUQ8uqCt7N7iTKoGuOhHSwRRwdtd");
 
-           // response
-           var repsone = restClient.Get(request);
+            // response
+            var repsone = restClient.Get(request);
+            string statusCode = repsone.StatusCode.ToString();
+
+            // Assert
+            Assert.Equal("OK", statusCode);
         }
     }
 }
