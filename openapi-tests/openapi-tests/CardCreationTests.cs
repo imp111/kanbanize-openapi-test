7edef95fe3ed3e4c5670568b06c371b1714635af
imp111 <alexsandartenev@gmail.com>
2023-04-20 07:35:55 +0300

    url segment added

--- openapi-tests/openapi-tests/CardCreationTests.cs
+++ openapi-tests/openapi-tests/CardCreationTests.cs
@@ -1,10 +1,6 @@
-using RestSharp;
-using RestSharp.Authenticators;
-using RestSharp.Authenticators.OAuth2;
-
 namespace openapi_tests
 {
-    public class UnitTest1
+    public class CardCreationTests
     {
         [Fact]
         public void Test1()
@@ -16,7 +12,9 @@ namespace openapi_tests
             });
 
             // request
-            var request = new RestRequest("/workspaces/");
+            var request = new RestRequest("/workspaces/{workspace_id}")
+                .AddUrlSegment("workspace_id", 2);
+
             request.AddHeader("apikey", "3ZIPG0qqf7fBuUQ8uqCt7N7iTKoGuOhHSwRRwdtd");
 
            // response
--- openapi-tests/openapi-tests/Usings.cs
+++ openapi-tests/openapi-tests/Usings.cs
@@ -1,1 +1,5 @@
 global using Xunit;
+global using RestSharp;
+global using RestSharp.Authenticators;
+global using FluentAssertions;
+global using OpenQA.Selenium;
